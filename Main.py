1f290de9f9d79815c1a6477804e7f8bd48bccd93
Mr.leonard <listas036@gmail.com>
2023-01-19 20:25:10 -0300

    V_1.5

--- Main.py
+++ Main.py
@@ -5,28 +5,31 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-# v Import the modules that will be used
-LB = LabelEncoder()                 # v Will only get the three closest datas
+def Clear():
+    system('clear')
+    system('cls')
+
+LE = LabelEncoder()                 # v Will only get the three closest datas
 KNN = KNeighborsClassifier(n_neighbors=3)
 RFC = RandomForestClassifier()
 
 SYMPTOMS = pd.read_csv('SYMPTOMS.csv') # < Get the data from the CSV file
-X1 = SYMPTOMS.drop('Result', axis=1)
-Y1 = SYMPTOMS.Result
+Input_1 = SYMPTOMS.drop('Result', axis=1)
+Output_1 = SYMPTOMS.Result
 
 # v Transform the data for the machine training
-X1['skin_tone'] = LB.fit_transform(X1['Old_skin_tone'])
-X1['eye_color'] = LB.fit_transform(X1['Old_eye_color'])
-X1['mental_health'] = LB.fit_transform(X1['Old_mental_health'])
-X1 = X1.drop(['Old_skin_tone', 'Old_eye_color', 'Old_mental_health'], axis=1)
-RFC.fit(X1, Y1) # < Make the machine training
+Input_1['New_Skin_Tone'] = LE.fit_transform(X1['Skin_tone'])
+Input_1['New_Eye_Color'] = LE.fit_transform(X1['Eye_color'])
+Input_1['New_Mental_Health'] = LE.fit_transform(X1['Mental_Health'])
+Input_1 = Input_1.drop(['Skin_tone', 'Eye_color', 'Mental_Health'], axis=1)
+RFC.fit(Input_1, Output_1) # < Make the machine training
 
 TYPE = pd.read_csv('TYPE.csv')
-X2 = TYPE.drop('Result', axis=1)
-Y2 = TYPE.Result
-KNN.fit(X2, Y2)
+Input_2 = TYPE.drop('Result', axis=1)
+Output_2 = TYPE.Result
+KNN.fit(Input_2, Output_2)
 
-system('cls')
+Clear()
 
 print('[Coding_Foundation] - [Z-Classifier]\n')
 sleep(1)
@@ -36,9 +39,9 @@ print('[1] Grey')
 sleep(0.5)
 print('[2] White\n')
 sleep(1)
-ST = int(input("[NUMBER] What is the patient's skin tone?: "))
+Skint_Tone = int(input("[NUMBER] What is the patient's skin tone?: "))
 
-system('cls')
+Clear()
 
 print('[Coding_Foundation] - [Z-Classifier]\n')
 sleep(1)
@@ -50,9 +53,9 @@ print('[2] Red')
 sleep(0.5)
 print('[3] White\n')
 sleep(1)
-YC = int(input("[NUMBER] What is the patient's eye color?: "))
+Eye_Color = int(input("[NUMBER] What is the patient's eye color?: "))
 
-system('cls')
+Clear()
 
 print('[Coding_Foundation] - [Z-Classifier]\n')
 sleep(1)
@@ -62,17 +65,17 @@ print('[1] Good')
 sleep(0.5)
 print('[2] Horrible\n')
 sleep(1)
-MH = int(input("[NUMBER] What is the patient's mental health?: "))
+Mental_Health = int(input("[NUMBER] What is the patient's mental health?: "))
 
-system('cls')   # v Will classify the data
-RESULT = RFC.predict([[ST,YC,MH]])
-system('cls')
+Clear()        # v Will classify the data
+RESULT = RFC.predict([[Skint_Tone,Eye_Color,Mental_Health]])
+Clear()
 
 print('[Coding_Foundation] - [Z-Classifier]\n')
 sleep(1)
 print(f'Result[{RESULT[0]}]')
 sleep(0.5)          # v Show the precision of the machine
-print(f'Precision[{RFC.score(X1, Y1)}]\n')
+print(f'Precision[{RFC.score(Input_1, Output_1)}]\n')
 sleep(1)
 if RESULT[0] == 'Zombie':
     input('[NUMBER/STRING] Continue: ')
@@ -85,28 +88,31 @@ elif RESULT[0] == 'Human':
     print('The patient is not infected...')
     exit()
 
-system('cls')
+Clear()
 
 print('[Coding_Foundation] - [Z-Classifier]\n')
 sleep(1)
-TL = float(input("[NUMBER] What is the zombie's teeth length?: "))
-if TL > 3.5: TL = 3.5
+Teeth = float(input("[NUMBER] What is the zombie's teeth length?: "))
+if Teeth > 3.5:
+    Teeth = 3.5
 sleep(0.5)
-NL = float(input("[NUMBER] What is the zombie's nails length?: "))
-if NL > 4.5: NL = 4.5
+Nails = float(input("[NUMBER] What is the zombie's nails length?: "))
+if Nails > 4.5:
+    Nails = 4.5
 sleep(0.5)
-EW = float(input("[NUMBER] What is the zombie's ears width?: "))
-if EW > 8.5: EW = 8.5
+Ears_and_Width = float(input("[NUMBER] What is the zombie's ears width?: "))
+if Ears_and_Width > 8.5:
+    Ears_and_Width = 8.5
 sleep(0.5)
 
-system('cls')
-RESULT = KNN.predict([[TL,NL,EW]])
-system('cls')
+Clear()
+RESULT = KNN.predict([[Teeth,Nails,Ears_and_Width]])
+Clear()
 
 print('[Coding_Foundation] - [Z-Classifier]\n')
 sleep(1)
 print(f'Result[{RESULT[0]}]')
 sleep(0.5)
-print(f'Precision[{KNN.score(X2, Y2)}]\n')
+print(f'Precision[{KNN.score(Input_2, Output_2)}]\n')
 sleep(1)
 print('[Contain process starting]')
